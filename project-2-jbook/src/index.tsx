4c6b4bb0dd9fbf39a100c5e133ec53cd34c06c9b
John Paul Lim Gabule <lucas.gabule@gmail.com>
2021-09-16 18:29:02 +0800

    Implementing In-Browser Bundling

---
+++ project-2-jbook/src/index.tsx
@@ -0,0 +1,46 @@
+import * as esbuild from 'esbuild-wasm'
+import {useEffect, useRef, useState} from 'react'
+import {render} from 'react-dom'
+import {unpkgPathPlugin} from "./plugins/unpkg-path-plugin";
+
+const App = () => {
+    const [input, setInput] = useState('');
+    const [code, setCode] = useState('');
+    const ref = useRef<any>();
+
+    useEffect(() => {
+        startService()
+    }, [])
+
+    const startService = async() => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        })
+    }
+
+    const onClick = async () => {
+        if (!ref.current) {
+            return;
+        }
+
+        const result = await ref.current.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()]
+        });
+        console.log(result)
+       setCode(result.outputFiles[0].text)
+    }
+
+    return <div>
+        <textarea onChange={e => setInput(e.target.value)} rows={15} cols={90} value={input} />
+        <div>
+            <button onClick={onClick}>Submit</button>
+        </div>
+        <pre>{code}</pre>
+    </div>
+}
+
+render(<App />, document.getElementById('root'))
